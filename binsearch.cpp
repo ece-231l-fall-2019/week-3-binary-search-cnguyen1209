a01df873f824a89144e6a95f9b520444272057d4
Cong Nguyen <cnguyen1209@unm.edu>
2019-09-12 11:04:35 -0600

    Test code

--- binsearch.cpp
+++ binsearch.cpp
@@ -3,15 +3,32 @@
 
 bool binarySearch(const std::vector<int>& set, int value)
 {
-	// TODO:
-	// implement me
+	size_t left = 0;
+	size_t right = set.size() - 1;
+	while (left <= right)
+	{
+			size_t mid = (left + right)/2;
+			if (set[mid] == value)
+						return true;
+			if (set[mid] > value)
+						right = mid -1;
+			else
+						left = mid +1;
+	}
 	return false;
 }
 
 bool binarySearch(const int *begin, const int *end, int value)
 {
-	// TODO:
-	// implement me
+		while (begin <= end)
+		{
+			const int *mid = begin + (end - begin)/2;
+			if (*mid == value)
+			return true;
+		if (*mid < value)
+					begin = mid + 1;
+			else
+					end = mid - 1;
+		}
 	return false;
 }
-
--- runtests.cpp
+++ runtests.cpp
@@ -1,15 +1,31 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
-
 #include "Timer.h"
 #include "search.h"
+using namespace std;
+
+void readFile(string filename, vector<int>& set)
+{
+	ifstream fin(filename);
+	while(true)
+	{
+		int num;
+		fin >> num;
+		if (fin.eof())
+			break;
+		set.push_back(num);
+	}
+	fin.close();
+}
 
 int main()
 {
 	std::vector<int> numbers;
 	std::vector<int> search;
 
+	readFile("numbers", numbers);
+	readFile("search", search);
 	// TODO:
 	// read the file "numbers" into the numbers vector
 	// read the file "search" into the search vector
@@ -46,9 +62,33 @@ int main()
 			<< search.size() << " values." << std::endl;
 	}
 
-	// TODO:
-	// repeat the above two blocks but use the binary search functions.
+	{
+		Timer timer("Time to binary search all values: ");
+
+		int found = 0;
+		for (size_t i = 0; i < search.size(); i++)
+		{
+			if (binarySearch(numbers, search[i]))
+				found++;
+		}
+
+		std::cout << "Found "<< found << "/"
+			<< search.size() << " values." << std::endl;
+	}
+
+	{
+		Timer timer("Time to binary search all values (pointers): ");
+
+		int found = 0;
+		for (size_t i = 0; i < search.size(); i++)
+		{
+			if (binarySearch(numbers.data(), numbers.data() + numbers.size(), search[i]))
+				found++;
+		}
+
+		std::cout << "Found "<< found << "/"
+			<< search.size() << " values." << std::endl;
+	}
 
 	return 0;
 }
-
