623b39821aef2c66a5638f14b8d10e3518d6881a
Cong Nguyen <cnguyen1209@unm.edu>
2019-09-14 13:24:11 -0600

    Add Bubble sort, edit Makefile, add function into runtests.cpp

--- sort.cpp
+++ sort.cpp
@@ -1,27 +1,21 @@
 #include <vector>
 #include "sort.h"
-/*void swap(int *a, int *b)
+void swap(int *a, int *b)
 {
-	int tmp;
-	tmp = *a;
+	int temp;
+	temp = *a;
 	*a = *b;
-	*b = tmp;
-}*/
+	*b = temp;
+}
 
 void bubblesort(int *begin, const int *end)
 {
-  int i,j;
-  int temp;
-  for( i = 1; i < 1999; i++)
+  for(int i = 1; i < 1999; i++)
   {
-    for( j = 0; j < 1999 - i; j++)
+    for(int j = 0; j < 1999 - i; j++)
     {
-      if (*(begin+j) > *(begin+j+1)) //&& ((begin+1) < end))
-        {
-          temp             = *(begin + j);
-          *(begin + j)     = *(begin + j + 1);
-          *(begin + j + 1) = temp;
-        }
-      }
+      if (*(begin+j) > *(begin+j+1))
+        swap((begin+j),(begin+j+1));
+    }
   }
 }
